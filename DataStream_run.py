02c14838b7ed333facb8423695d0b9e83e1ac484
Limitless <110753007+Gry-Spark@users.noreply.github.com>
2022-11-30 13:39:54 +0300

    Update DataStream_run.py

--- DataStream_run.py
+++ DataStream_run.py
@@ -26,7 +26,6 @@ from PySide2.QtWidgets import *
 import binance.client
 from binance.client import Client
 
-st.set_page_config(layout="wide")
 
 #Binance api
 Pkey = ''
@@ -54,7 +53,13 @@ intevral = Client.KLINE_INTERVAL_1HOUR ; depth = '4 days ago UTC+3'
 
 intevral_4 = Client.KLINE_INTERVAL_5MINUTE ; depth_4 = '2500 minutes ago UTC+3'
 
-tickers = ['fdfdfd','BTCUSDT', 'ETHUSDT']
+tickers = ['BTCUSDT','NBSUSDT','ATOMUSDT']
+
+
+col1 = st.columns(1)
+
+
+length = st.slider("Tps at less", value=1,max_value=100)
 
 for ticker in tickers:
     try:
@@ -73,7 +78,7 @@ for ticker in tickers:
             df ["Trades_Count"] = pd.to_numeric(df["Trades_Count"])
             df ["asset"] = ticker
             avg_price = client.get_avg_price(symbol=ticker)
-            avg_price = round(float(avg_price['price']),2)                                                     
+            avg_price = round(float(avg_price['price']),2)
             #Buy/Sell volume per succend
             #df ["SELL_VOL"]    = (df.Volume -df.BUY_VOL)
             #df ["BTS"]    = round(df.BUY_VOL /3600,2)
@@ -103,100 +108,104 @@ for ticker in tickers:
             TPS_1h = df["TPS_1h"][94]
             TPS_1h_Live = df["TPS_1h"][95]
             TPS_chg_1H = df[f'TPS_chg_1H'][94]
-            Score48_1h = df['vwap_zscore48'][94]               
+            Score48_1h = df['vwap_zscore48'][94]
             ATS_1h = df["ATS_1h"][94]
             ATS_chg_1H = df[f'ATS_chg_1H'][94]
             SortData_1h = df["SortData_1h"][94]
             ticker_1h = df['asset'][94]
+            
+            if length < TPS_1h:
 
-            price_chg_list.append(Price_1H)
-            TPS_1h_list.append(TPS_1h)
-            TPS_1H_LIVE_LISt.append(TPS_1h_Live)
-            TPS_chg_1H_list.append(TPS_chg_1H)
-            Score48_1h_list.append(Score48_1h)            
-            ATS_1h_list.append(ATS_1h)
-            ATS_chg_1H_list.append(ATS_chg_1H)
-            SortData_1h_list.append(SortData_1h)
-            coin_list_1h.append(ticker_1h)
-
-            df_4 = client.get_historical_klines(ticker , intevral_4 , depth_4)
-            df_4 = pd.DataFrame(df_4)
-            if not df_4.empty:
-                df_4[0] = pd.to_datetime(df_4[0],unit='ms')
-                df_4.columns = ['Date','Open','High','Low','Close','Volume','IGNORE','Quote_Volume','Trades_Count','BUY_VOL','BUY_VOL_VAL','x']
-                df_4 ["Open"] = pd.to_numeric(df_4["Open"])
-                df_4 ["High"] = pd.to_numeric(df_4["High"])
-                df_4 ["Low"] = pd.to_numeric(df_4["Low"])
-                df_4 ["Close"] = pd.to_numeric(df_4["Close"])
-                df_4 ["Volume"] = round(pd.to_numeric(df_4["Volume"]))
-                df_4 ["BUY_VOL"] = round(pd.to_numeric(df_4["BUY_VOL"]))
-                df_4 ["Quote_Volume"] = round(pd.to_numeric(df_4["Quote_Volume"]))
-                df_4 ["Trades_Count"] = pd.to_numeric(df_4["Trades_Count"])  
-                #df_4.set_index("Date" , inplace=True)
-                tpss = df_4 ["Trades_Count"][498]
-                TPSS =  tpss / 60
-
-                df_4['vc'] = (( df_4.Close + df_4.Low + df_4.High) / 3) * df_4.Volume 
-                df_4['vwap48']=round((df_4['vc'].rolling(window=48).sum())/(df_4['Volume'].rolling(window=48).sum()),100).fillna(method='bfill')
-                df_4['vwap84']=round((df_4['vc'].rolling(window=84).sum())/(df_4['Volume'].rolling(window=84).sum()),100).fillna(method='bfill')
-
-                df_4['v48_01'] = (df_4.Close - df_4.vwap48) 
-                df_4['v488_02'] = round((df_4.v48_01 / df_4.Close) *100 , 2)
-
-                v48_Space = df_4 ["v488_02"][498]
-
-                MAHMAD_LISt.append(TPSS)
-                v48_Space_list.append(v48_Space)
-                    #price_chg_list.append(Price_1H)
-
-
-
-
-                dfc = pd.DataFrame()
-                dfc['Coin'] = coin_list_1h
-                dfc['1H Live %'] = price_chg_list  
-                dfc['1hr_tps'] = TPS_1h_list 
-                dfc['⚡️W48 5min⚡️'] = v48_Space_list
-                dfc['Score48_1h'] = Score48_1h_list
-                dfc['TPS_chg_1H'] = TPS_chg_1H_list 
-                dfc['TPS_1H_LIVE'] = TPS_1H_LIVE_LISt
-
-                dfc['TPS_5m'] = MAHMAD_LISt
-                dfc['🎁Special TPS🎁'] = dfc['⚡️W48_Space5m⚡️'] / dfc['TPS_1h']
-                dfc['ATS_1h_list'] = ATS_1h_list
-                dfc['ATS_chg_1H_list'] = ATS_chg_1H_list
-
-                
-                dfc['Score48_1h'] = Score48_1h_list
-                dfc['SortData_final1h'] = SortData_1h_list
+                price_chg_list.append(Price_1H)
+                TPS_1h_list.append(TPS_1h)
+                TPS_1H_LIVE_LISt.append(TPS_1h_Live)
+                TPS_chg_1H_list.append(TPS_chg_1H)
+                Score48_1h_list.append(Score48_1h)
+                ATS_1h_list.append(ATS_1h)
+                ATS_chg_1H_list.append(ATS_chg_1H)
+                SortData_1h_list.append(SortData_1h)
+                coin_list_1h.append(ticker_1h)
                 
                 
+                df_4 = client.get_historical_klines(ticker , intevral_4 , depth_4)
+                df_4 = pd.DataFrame(df_4)
+                if not df_4.empty:
+                    df_4[0] = pd.to_datetime(df_4[0],unit='ms')
+                    df_4.columns = ['Date','Open','High','Low','Close','Volume','IGNORE','Quote_Volume','Trades_Count','BUY_VOL','BUY_VOL_VAL','x']
+                    df_4 ["Open"] = pd.to_numeric(df_4["Open"])
+                    df_4 ["High"] = pd.to_numeric(df_4["High"])
+                    df_4 ["Low"] = pd.to_numeric(df_4["Low"])
+                    df_4 ["Close"] = pd.to_numeric(df_4["Close"])
+                    df_4 ["Volume"] = round(pd.to_numeric(df_4["Volume"]))
+                    df_4 ["BUY_VOL"] = round(pd.to_numeric(df_4["BUY_VOL"]))
+                    df_4 ["Quote_Volume"] = round(pd.to_numeric(df_4["Quote_Volume"]))
+                    df_4 ["Trades_Count"] = pd.to_numeric(df_4["Trades_Count"])  
+                    #df_4.set_index("Date" , inplace=True)
+                    tpss = df_4 ["Trades_Count"][498]
+                    TPSS =  tpss / 60
 
-                df ["price_chg_Live"] = pd.to_numeric(df["price_chg_Live"])
-                df ["TPS_1h"] = pd.to_numeric(df["TPS_1h"])
-                df ["⚡️W48_Space5m⚡️"] = pd.to_numeric(df["⚡️W48_Space5m⚡️"])
-                df ["Score48_1h"] = pd.to_numeric(df["Score48_1h"])
-                df ["TPS_chg_1H"] = pd.to_numeric(df["TPS_chg_1H"])
-                df ["TPS_1H_LIVE"] = pd.to_numeric(df["TPS_1H_LIVE"])
-                df ["TPS_5m"] = pd.to_numeric(df["TPS_5m"])
-                df ["🎁Special TPS🎁"] = pd.to_numeric(df["🎁Special TPS🎁"])
-                df ["ATS_1h_list"] = pd.to_numeric(df["ATS_1h_list"])
-                df ["ATS_chg_1H_list"] = pd.to_numeric(df["ATS_chg_1H_list"])
+                    df_4['vc'] = (( df_4.Close + df_4.Low + df_4.High) / 3) * df_4.Volume 
+                    df_4['vwap48']=round((df_4['vc'].rolling(window=48).sum())/(df_4['Volume'].rolling(window=48).sum()),100).fillna(method='bfill')
+                    df_4['vwap84']=round((df_4['vc'].rolling(window=84).sum())/(df_4['Volume'].rolling(window=84).sum()),100).fillna(method='bfill')
 
-                df ["SortData_final1h"] = pd.to_numeric(df["SortData_final1h"])      
-                     
-              
-                #Price 4H
-                df_4 ["Price_4H"] = round(df_4.Close.pct_change(1)*100,2).fillna(0) 
-                #TPS
-                df_4 ["TPS"]    = round(df.Trades_Count /14400,2) 
-                df_4 ["TPS_chg_4H"] = round(df_4.TPS.pct_change(1)*100,2).fillna(0) 
-                #ATS  
-                df_4 ['ATS'] = df_4 ["Volume"] / df_4 ["Trades_Count"]
-                df_4 ["ATS_chg_4H"] = round(df_4.ATS.pct_change(1)*100,2).fillna(0) 
-                #Volume
-                df_4 ["Vol_chg_4H"] = round(df_4.Volume.pct_change(1)*100,2).fillna(0) 
-                df_4 ['SortData']  = df_4.TPS / df_4.Price_4H
+                    df_4['v48_01'] = (df_4.Close - df_4.vwap48) 
+                    df_4['v488_02'] = round((df_4.v48_01 / df_4.Close) *100 , 2)
+
+                    v48_Space = df_4 ["v488_02"][498]
+
+                    MAHMAD_LISt.append(TPSS)
+                    v48_Space_list.append(v48_Space)
+                        #price_chg_list.append(Price_1H)
+
+
+
+                    dfc = pd.DataFrame()
+                    dfc['coin_list_1h'] = coin_list_1h
+                    dfc['price_chg_Live'] = price_chg_list  
+                    dfc['TPS_1h'] = TPS_1h_list 
+                    dfc['⚡️W48_Space5m⚡️'] = v48_Space_list
+                    dfc['Score48_1h'] = Score48_1h_list
+                    dfc['TPS_chg_1H'] = TPS_chg_1H_list 
+                    dfc['TPS_1H_LIVE'] = TPS_1H_LIVE_LISt
+
+                    dfc['TPS_5m'] = MAHMAD_LISt
+                    dfc['🎁Special TPS🎁'] = dfc['TPS_5m'] * dfc['TPS_1h']
+                    dfc['ATS_1h_list'] = ATS_1h_list
+                    dfc['ATS_chg_1H_list'] = ATS_chg_1H_list
+
+
+                    dfc['Score48_1h'] = Score48_1h_list
+                    dfc['SortData_final1h'] = SortData_1h_list
+
+
+
+                    df ["price_chg_Live"] = pd.to_numeric(df["price_chg_Live"])
+                    df ["TPS_1h"] = pd.to_numeric(df["TPS_1h"])
+                    df ["⚡️W48_Space5m⚡️"] = pd.to_numeric(df["⚡️W48_Space5m⚡️"])
+                    df ["Score48_1h"] = pd.to_numeric(df["Score48_1h"])
+                    df ["TPS_chg_1H"] = pd.to_numeric(df["TPS_chg_1H"])
+                    df ["TPS_1H_LIVE"] = pd.to_numeric(df["TPS_1H_LIVE"])
+                    df ["TPS_5m"] = pd.to_numeric(df["TPS_5m"])
+                    df ["🎁Special TPS🎁"] = pd.to_numeric(df["🎁Special TPS🎁"])
+                    df ["ATS_1h_list"] = pd.to_numeric(df["ATS_1h_list"])
+                    df ["ATS_chg_1H_list"] = pd.to_numeric(df["ATS_chg_1H_list"])
+
+                    df ["SortData_final1h"] = pd.to_numeric(df["SortData_final1h"])      
+
+
+                    #Price 4H
+                    df_4 ["Price_4H"] = round(df_4.Close.pct_change(1)*100,2).fillna(0) 
+                    #TPS
+                    df_4 ["TPS"]    = round(df.Trades_Count /14400,2) 
+                    df_4 ["TPS_chg_4H"] = round(df_4.TPS.pct_change(1)*100,2).fillna(0) 
+                    #ATS  
+                    df_4 ['ATS'] = df_4 ["Volume"] / df_4 ["Trades_Count"]
+                    df_4 ["ATS_chg_4H"] = round(df_4.ATS.pct_change(1)*100,2).fillna(0) 
+                    #Volume
+                    df_4 ["Vol_chg_4H"] = round(df_4.Volume.pct_change(1)*100,2).fillna(0) 
+                    df_4 ['SortData']  = df_4.TPS / df_4.Price_4H
+            else :
+                pass        
     except:
         pass
                 
@@ -223,7 +232,6 @@ def color_negative_values(value):
        color = '#989ba2'
    return f"color: {color}"
 
-# Using Style for the Dataframe
 
-expander_bar = st.expander("TPS")
-expander_bar.dataframe(dff.style.applymap(color_negative_values , subset=['price_chg_Live','TPS_1h','⚡️W48_Space5m⚡️','Score48_1h','TPS_chg_1H','TPS_1H_LIVE','TPS_5m','🎁Special TPS🎁','ATS_1h_list','ATS_chg_1H_list','SortData_final1h']).applymap(lambda x: 'font-size:23.2px; background-color: #131c25' ))
+# Using Style for the Dataframe
+st.dataframe(dff.style.applymap(color_negative_values , subset=['price_chg_Live','TPS_1h','⚡️W48_Space5m⚡️','Score48_1h','TPS_chg_1H','TPS_1H_LIVE','TPS_5m','🎁Special TPS🎁','ATS_1h_list','ATS_chg_1H_list','SortData_final1h']).applymap(lambda x: 'font-size:23.2px; background-color: #131c25' ))
